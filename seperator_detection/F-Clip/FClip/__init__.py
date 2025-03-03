1853fd21e13b1bebe8aba44786a8cc304f703e14
Wenjun_SUN <sunwenjun1997@gmail.com>
2025-03-03 11:52:32 +0100

    Delete seperator_detection/F-Clip directory

--- seperator_detection/F-Clip/FClip/__init__.py
+++
@@ -1,8 +0,0 @@
-import FClip.models
-import FClip.trainer
-import FClip.datasets
-import FClip.config
-import FClip.lr_schedulers
-import FClip.visualize
-import FClip.postprocess
-import FClip.line_parsing
